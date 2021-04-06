a700312cbef3b67fafa8763e58f6a93e2d8707ca
gabrielztk <gabrielztk@al.insper.edu.br>
2021-04-06 14:16:53 -0300

    Vocab

--- common_words.py
+++ common_words.py
@@ -8,20 +8,27 @@ from tqdm import tqdm
 from collections import Counter
 
 # Lê o arquivo com as palavras
+
+
 def read_data(filename):
     with open(filename, 'r') as file:
         data = [json.loads(line) for line in file]
     return data
 
 # Limpa as palavras
+
+
 def minusculas(tokens):
     return [token.lower() for token in tokens]
+
 
 def remove_digitos(tokens):
     return [token for token in tokens if re.fullmatch('[^\d]*', token)]
 
+
 def pega_palavras(tokens):
     return [token for token in tokens if re.fullmatch('\w+', token)]
+
 
 def stop_words(tokens):
     stop_words = set(stopwords.words('portuguese'))
@@ -29,25 +36,33 @@ def stop_words(tokens):
 
 # Remove alguns tipos de palavras, como artigos
 # https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+
+
 def tag_words(text):
     remove_tags = ['DT', 'TO', 'IN']
     tagged = pos_tag(text)
     return [tag[0] for tag in tagged if not tag[1] in remove_tags]
 
-# Limpa os tokens    
+# Limpa os tokens
+
+
 def limpa_tokens(tokens):
-    tokens = minusculas(tokens)
+    # tokens = minusculas(tokens)
     tokens = remove_digitos(tokens)
-    tokens = pega_palavras(tokens)    
+    tokens = pega_palavras(tokens)
     tokens = stop_words(tokens)
     return tokens
 
+
 def main():
-    data = read_data(Path('..', 'Aula 4', 'dump_small.jsonln'))
+
+    arquivo = 'dump_small_clean.jsonln'
+    with open(arquivo, 'r') as f:
+        data = list(json.load(f))
 
     all_words = []
     for item in tqdm(data):
-        texto = item['body']
+        texto = item
         tokens = word_tokenize(texto)
         tokens = limpa_tokens(tokens)
         all_words += tokens
@@ -55,12 +70,17 @@ def main():
     tagged_words = tag_words(all_words)
 
     word_counts_list = list(Counter(tagged_words).items())
-    word_counts_list_sorted = sorted(word_counts_list, key=lambda x: (-x[1], x[0]))
+    word_counts_list_sorted = sorted(
+        word_counts_list, key=lambda x: (-x[1], x[0]))
 
     vocab = dict(word_counts_list_sorted[0:3000])
 
+    with open("vocab.jsonln", "w") as fp:
+        json.dump(vocab, fp)
+
     print(vocab)
     return vocab
+
 
 if __name__ == '__main__':
     main()
